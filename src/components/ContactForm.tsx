7d684557c0aedc5a4da6d8f193c0041b67ce0b82
Ardagecimli0 <ardagec0@gmail.com>
2026-02-16 04:45:25 +0300

    ddd

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -161,7 +161,7 @@ export default function ContactForm() {
       console.log("Sending to Zoho:", payload);
 
       // Submit the form data to the API
-      const response = await fetch(`https://cevre.hotelistan.net/api/form-patient`, {
+      const response = await fetch(`https://zoho.hotelistan.net/api/form-patient`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
